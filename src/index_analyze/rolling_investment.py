6951af7313d76852d21df06252b5fd3d50446bdb
Jens Carlén <jens.carlen@offerta.se>
2024-01-25 20:47:46 +0100

    analyze_matrix

--- src/index_analyze/rolling_investment.py
+++ src/index_analyze/rolling_investment.py
@@ -1,27 +1,33 @@
 import os
-import pandas as pd
 from pathlib import Path
+from datetime import datetime
+import pandas as pd
+
 
 def merge(contents):
     data = pd.DataFrame()
     for c in contents:
-         data = pd.merge(data, c, how="outer", left_index=True, right_index=True)
+        data = pd.merge(data, c, how="outer", left_index=True, right_index=True)
 
     return data
+
 
 def run(directory):
     contents = []
 
     for filename in os.listdir(directory):
-        if filename.endswith('.csv'):
+        if filename.endswith(".csv"):
             with open(os.path.join(directory, filename)) as f:
-                contents.append(pd.read_csv(f, parse_dates=['Date'], date_format="%Y-%m").set_index('Date'))
-                # data = pd.merge(data, content, how="outer", left_index=True, right_index=True)
-    data = merge(contents)
+                contents.append(pd.read_csv(f, parse_dates=["Date"], date_format="%Y-%m").set_index("Date"))
+    data = build_analyze_matrix(merge(contents))
     print(data)
 
-def analyze(data):
-    pass
+
+def build_analyze_matrix(data, years = 5):
+    values_5_years_ago = data.shift(periods=1, freq=pd.DateOffset(years=years))
+    result = data.div(values_5_years_ago)
+    return result.dropna(how="all")
+
 
 if __name__ == "__main__":
-    run(Path("./data/curvo"))
+    run(Path("./data/monthly"))
--- tests/test_rolling_investment.py
+++ tests/test_rolling_investment.py
@@ -1,22 +1,58 @@
-import pytest
-from index_analyze.rolling_investment import merge
 import pandas as pd
 
+from index_analyze.rolling_investment import build_analyze_matrix, merge
+
+
 def test_merge():
-    a = pd.DataFrame(data=[{'a': 1}])
-    b = pd.DataFrame(data=[{'b': 2}])
-    assert merge([a,b]).equals(pd.DataFrame(data=[{'a': 1, 'b': 2}]))
+    a = pd.DataFrame(data=[{"a": 1}])
+    b = pd.DataFrame(data=[{"b": 2}])
+    assert merge([a, b]).equals(pd.DataFrame(data=[{"a": 1, "b": 2}]))
+
 
 def test_merge_multiple():
-    a = pd.DataFrame(data=[{'a': 1}])
-    b = pd.DataFrame(data=[{'b': 2}])
-    c = pd.DataFrame(data=[{'c': 3}])
-    assert merge([a,b,c]).equals(pd.DataFrame(data=[{'a': 1, 'b': 2, 'c': 3}]))
+    a = pd.DataFrame(data=[{"a": 1}])
+    b = pd.DataFrame(data=[{"b": 2}])
+    c = pd.DataFrame(data=[{"c": 3}])
+    assert merge([a, b, c]).equals(pd.DataFrame(data=[{"a": 1, "b": 2, "c": 3}]))
+
 
 def test_merge_with_index():
-    a = pd.DataFrame(data=[{'i': 0, 'a': 1}]).set_index('i')
-    b = pd.DataFrame(data=[{'i': 1, 'b': 2}]).set_index('i')
-    c = pd.DataFrame(data=[{'i': 0, 'c': 3}]).set_index('i')
+    a = pd.DataFrame(data=[{"i": 0, "a": 1}]).set_index("i")
+    b = pd.DataFrame(data=[{"i": 1, "b": 2}]).set_index("i")
+    c = pd.DataFrame(data=[{"i": 0, "c": 3}]).set_index("i")
 
-    excepted = pd.DataFrame(data=[{'a': 1, 'c': 3}, {'b': 2}], columns=['a', 'b', 'c'])
-    assert merge([a,b,c]).equals(excepted)
+    excepted = pd.DataFrame(data=[{"a": 1, "c": 3}, {"b": 2}], columns=["a", "b", "c"])
+    assert merge([a, b, c]).equals(excepted)
+
+
+def test_analyze_matrix():
+    data = [
+        {"date": pd.Timestamp("2010-01-01"), "a": 1.0, "b": 1.0, "c": 1.0},
+        {"date": pd.Timestamp("2015-01-01"), "a": 1.0, "b": 2.0, "c": 3.0}
+    ]
+
+    source = pd.DataFrame(data=data).set_index("date")
+    result = build_analyze_matrix(source)
+
+    assert result.shape == (1,3)
+    assert result.loc[pd.Timestamp("2015-01-01")].loc["a"] == 1.0
+    assert result.loc[pd.Timestamp("2015-01-01")].loc["b"] == 2.0
+    assert result.loc[pd.Timestamp("2015-01-01")].loc["c"] == 3.0
+
+
+def test_analyze_matrix_with_more_rows():
+    data = [
+        {"date": pd.Timestamp("2010-01-01"), "a": 1.0, "b": 1.0, "c": 1.0},
+        {"date": pd.Timestamp("2009-01-01"), "a": 1.0, "b": 1.0, "c": 1.0},
+        {"date": pd.Timestamp("2015-01-01"), "a": 1.0, "b": 2.0, "c": 3.0},
+        {"date": pd.Timestamp("2020-01-01"), "a": 1.0, "b": 2.0, "c": 3.0}
+
+    ]
+
+    source = pd.DataFrame(data=data).set_index("date")
+    result = build_analyze_matrix(source)
+    print(result)
+    assert result.shape == (2,3)
+    assert result.loc[pd.Timestamp("2020-01-01")].loc["a"] == 1.0
+    assert result.loc[pd.Timestamp("2020-01-01")].loc["b"] == 1.0
+    assert result.loc[pd.Timestamp("2020-01-01")].loc["c"] == 1.0
