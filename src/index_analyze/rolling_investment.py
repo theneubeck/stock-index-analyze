7b89dc73fe6512c32b03478fd7f04843527b9219
Jens Carlén <jens.carlen@offerta.se>
2024-01-27 16:34:20 +0100

    Mean of all years

--- src/index_analyze/rolling_investment.py
+++ src/index_analyze/rolling_investment.py
@@ -14,28 +14,43 @@ def merge(contents):
 
 def run(directory):
     contents = []
+    result = pd.DataFrame()
 
     for filename in os.listdir(directory):
         if filename.endswith(".csv"):
             with open(os.path.join(directory, filename)) as f:
                 contents.append(pd.read_csv(f, parse_dates=["Date"], date_format="%Y-%m").set_index("Date"))
-    data = build_analyze_matrix(merge(contents))
-    return analyze(data)
 
-def analyze(data):
-    means = pd.DataFrame(columns=["mean"], data=data.mean().transpose())
-    means["median"] = data.median().array
-    means["min"] = data.min().array
-    means["max"] = data.max().array
-    means["yearly"] = means["mean"].apply(lambda x: x**(1/5))
-    means["yearly_median"] = means["median"].apply(lambda x: x**(1/5))
-    return means.sort_values(by=["median"], ascending=False)
+    for years in range(1,11):
+        data = build_analyze_matrix(merge(contents), years)
+        result = pd.concat([result, analyze(data, years)])
+    return result.sort_values(by=["median"], ascending=False).dropna()
+
+def analyze(data, years):
+    analyze = pd.DataFrame(columns=["name"], data=data.columns)
+    analyze["years"] = years
+    analyze["mean"] = data.mean().array
+    analyze["median"] = data.median().array
+    analyze["min"] = data.min().array
+    analyze["max"] = data.max().array
+    analyze["yearly"] = analyze["mean"].apply(lambda x: x**(1/years))
+    analyze["yearly_median"] = analyze["median"].apply(lambda x: x**(1/years))
+    return analyze.sort_values(by=["median"], ascending=False)
 
 def build_analyze_matrix(data, years = 5):
     values_5_years_ago = data.shift(periods=1, freq=pd.DateOffset(years=years))
     result = data.div(values_5_years_ago)
     return result.dropna(how="all")
 
+def mean_of_all_years(data):
+    group = data.groupby("name")
+    analyze = pd.DataFrame()
+    analyze["yearly_mean"] = group["yearly"].mean()
+    analyze["yearly_median_mean"] = group["yearly_median"].mean()
+    return analyze.sort_values(by=["yearly_median_mean"], ascending=False)
 
 if __name__ == "__main__":
-    print(run(Path("./data/monthly")))
+    with pd.option_context('display.max_rows', None):
+        baz  = run(Path("./data/monthly"))
+        print(baz)
+        # print(mean_of_all_years(baz))
